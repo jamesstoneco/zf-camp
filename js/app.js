946a61890c3bce947ff226da2628cd440c960ecb
James M. Stone <jamesmanofstone@gmail.com>
2015-11-03 04:59:58 -0800

    added discount code passthrough for eventbrite

--- js/app.js
+++ js/app.js
@@ -1,5 +1,30 @@
 // $(document).foundation();
 
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+	sURLVariables = sPageURL.split('&'),
+	sParameterName,
+	i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
+};
+
+$(document).ready(function () {
+	if (getUrlParameter('discount')) {
+		var discount = getUrlParameter('discount');
+		var iFrameSrc = $('#eventbrite-iframe').attr('src');
+		$('#eventbrite-iframe').attr('src', iFrameSrc + "&discount=" + discount );
+	}
+});
+
+
+
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
